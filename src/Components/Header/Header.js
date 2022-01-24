e350fc839a454ace5f0f1365d0af93476800e6b1
octaviofitz <octaviofitz@gmail.com>
2022-01-24 03:59:11 -0300

    test vercel

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,7 +3,7 @@ import '../Header/header.css'
 function Header() {
   return (
     <header id='header'>
-      <span className='header-title'>Bentonita</span>    
+      <span className='header-title'>hola</span>    
     </header>
   );
 }
